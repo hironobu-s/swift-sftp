d13381fd89ee0bd0e222bba46a82d1334027eb4c
Hironobu Saitoh <hiro@hironobu.org>
2018-11-27 16:50:39 +0900

    initial commit

---
+++ swift.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/objectstorage/v1/containers"
+	"github.com/gophercloud/gophercloud/openstack/objectstorage/v1/objects"
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+type Swift struct {
+	config Config
+}
+
+func NewSwift(c Config) *Swift {
+	return &Swift{
+		config: c,
+	}
+}
+
+func (s *Swift) Init() error {
+	// Make sure whether the container exists
+	ojs, err := s.getOjsClient()
+	if err != nil {
+		return err
+	}
+
+	exists := false
+	containers.List(ojs, containers.ListOpts{}).EachPage(func(p pagination.Page) (bool, error) {
+		names, err := containers.ExtractNames(p)
+		if err != nil {
+			return false, err
+		}
+
+		for _, name := range names {
+			if name == s.config.Container {
+				exists = true
+				return false, nil
+			}
+		}
+
+		return true, nil
+	})
+
+	if !exists {
+		if s.config.CreateContainerIfNotExists {
+			if err = s.CreateContainer(); err != nil {
+				return fmt.Errorf("Couldn't create container. [%s]", err)
+			}
+		} else {
+			return fmt.Errorf("Container '%s' does not exist.", s.config.Container)
+		}
+	}
+
+	return nil
+}
+
+func (s *Swift) CreateContainer() (err error) {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return err
+	}
+
+	rs := containers.Create(client, s.config.Container, containers.CreateOpts{})
+	return rs.Err
+}
+
+func (s *Swift) DeleteContainer() (err error) {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return err
+	}
+
+	ls, err := s.List()
+	if err != nil {
+		return err
+	}
+
+	// Recursive deletion for all objects in the container
+	for _, obj := range ls {
+		drs := objects.Delete(client, s.config.Container, obj.Name, objects.DeleteOpts{})
+		if drs.Err != nil {
+			return drs.Err
+		}
+	}
+
+	rs := containers.Delete(client, s.config.Container)
+	return rs.Err
+}
+
+func (s *Swift) List() (ls []objects.Object, err error) {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ls = make([]objects.Object, 0, 10)
+	err = objects.List(client, s.config.Container, objects.ListOpts{
+		Full: true,
+	}).EachPage(func(p pagination.Page) (bool, error) {
+		ls, err = objects.ExtractInfo(p)
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	})
+
+	return ls, err
+}
+
+func (s *Swift) Get(name string) (header *objects.GetHeader, err error) {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return objects.Get(client, s.config.Container, name, objects.GetOpts{}).Extract()
+}
+
+func (s *Swift) Download(name string) (content io.ReadCloser, err error) {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return nil, err
+	}
+
+	rs := objects.Download(client, s.config.Container, name, objects.DownloadOpts{})
+	return rs.Body, nil
+}
+
+func (s *Swift) Put(name string, content io.Reader) error {
+	client, err := s.getOjsClient()
+	if err != nil {
+		return err
+	}
+
+	// temporary object name
+	tmpname := "tmp_" + name
+
+	// delete a temporary file from container
+	defer func() {
+		objects.Delete(client, s.config.Container, tmpname, objects.DeleteOpts{})
+	}()
+
+	cOpts := objects.CreateOpts{
+		Content: content,
+	}
+	rCreate := objects.Create(client, s.config.Container, tmpname, cOpts)
+	if rCreate.Err != nil {
+		return rCreate.Err
+	}
+
+	dest := fmt.Sprintf("%s/%s", s.config.Container, name)
+	rCopy := objects.Copy(client, s.config.Container, tmpname, objects.CopyOpts{
+		Destination: dest,
+	})
+	if rCopy.Err != nil {
+		return rCopy.Err
+	}
+
+	return nil
+}
+
+func (s *Swift) getOjsClient() (*gophercloud.ServiceClient, error) {
+	auth, err := s.getAuthClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return openstack.NewObjectStorageV1(auth, gophercloud.EndpointOpts{
+		Region: s.config.Region,
+	})
+}
+
+func (s *Swift) getAuthClient() (*gophercloud.ProviderClient, error) {
+	var (
+		err  error
+		opts gophercloud.AuthOptions
+	)
+
+	if (s.config.UserID != "" || s.config.Username != "") && s.config.Password != "" {
+		opts = gophercloud.AuthOptions{
+			IdentityEndpoint: s.config.IdentityEndpoint,
+			UserID:           s.config.UserID,
+			Username:         s.config.Username,
+			Password:         s.config.Password,
+			DomainID:         s.config.DomainID,
+			DomainName:       s.config.DomainName,
+			TenantID:         s.config.TenantID,
+			TenantName:       s.config.TenantName,
+		}
+
+	} else if opts, err = openstack.AuthOptionsFromEnv(); err != nil {
+		return nil, err
+	}
+
+	return openstack.AuthenticatedClient(opts)
+}
---
+++ swift_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/k0kubun/pp"
+)
+
+var (
+	testfile = "testfile.txt"
+	tmpfile  = "tmp_testfile.txt"
+	s        *Swift
+)
+
+func TestMain(m *testing.M) {
+	c := Config{
+		Container:                  "ojs-test-container",
+		CreateContainerIfNotExists: true,
+	}
+
+	// First, delete the container
+	s = NewSwift(c)
+	s.Init()
+	s.DeleteContainer()
+
+	// Recreate swift for testing
+	s = NewSwift(c)
+
+	// run
+	code := m.Run()
+
+	// after testing
+	s.DeleteContainer()
+	os.Remove(testfile)
+
+	os.Exit(code)
+}
+
+func TestAuthFromEnv(t *testing.T) {
+	if err := s.Init(); err != nil {
+		fmt.Printf("%v", err)
+		t.Fail()
+	}
+}
+
+func TestList(t *testing.T) {
+	ls, err := s.List()
+	if err != nil {
+		fmt.Printf("%v", err)
+		t.Fail()
+	}
+
+	if len(ls) > 0 {
+		pp.Printf("%v\n", ls)
+		t.Errorf("%d objects exists on the object storage and then the test result might be incorrect.", len(ls))
+	}
+}
+
+func TestPut(t *testing.T) {
+
+	data := []byte("This is test data.")
+	err := ioutil.WriteFile("testfile.txt", data, 0600)
+	if err != nil {
+		t.Errorf("%v", err)
+		t.Fail()
+	}
+
+	err = s.Put(testfile, bytes.NewReader(data))
+	if err != nil {
+		t.Errorf("%v", err)
+		t.Fail()
+	}
+
+	ls, err := s.List()
+	if err != nil {
+		t.Errorf("%v", err)
+		t.Fail()
+	}
+
+	existTestfile := false
+	for _, obj := range ls {
+		if obj.Name == testfile {
+			existTestfile = true
+		} else if obj.Name == tmpfile {
+			t.Errorf("Temporary file '%s' exists", tmpfile)
+			t.Fail()
+		}
+	}
+
+	if !existTestfile {
+		t.Errorf("Does not exist testfile. '%s'", testfile)
+	}
+}
+func TestGet(t *testing.T) {
+	header, err := s.Get(testfile)
+	if err != nil {
+		t.Errorf("%v\n", err)
+		t.Fail()
+	} else if header == nil {
+		t.Errorf("Couldn't get the header of the object")
+		t.Fail()
+	}
+}
+
+func TestDownload(t *testing.T) {
+	obj, err := s.Download(testfile)
+	if err != nil {
+		t.Errorf("%v\n", err)
+		t.Fail()
+	}
+
+	data1, _ := ioutil.ReadFile(testfile)
+	data2, _ := ioutil.ReadAll(obj)
+
+	if len(data1) != len(data2) {
+		t.Errorf("Size missmatched between the test data and downloaded content. [%d != %d]\n", len(data1), len(data2))
+		t.Fail()
+	}
+}
