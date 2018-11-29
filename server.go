610de592e1f5284b606a8eadd1b0f6ebe5658481
Hironobu Saitoh <hiro@hironobu.org>
2018-11-29 16:12:56 +0900

    Change configs

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"net"
+	"os"
+
+	"crypto/x509"
 
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh"
@@ -11,7 +18,7 @@ import (
 
 func StartServer(conf Config) error {
 	// Prepare server config
-	sConf, err := serverConfig(conf)
+	sConf, err := initServer(conf)
 	if err != nil {
 		return err
 	}
@@ -23,12 +30,11 @@ func StartServer(conf Config) error {
 	}
 
 	// Start server
-	listenAddr := fmt.Sprintf("%s:%d", conf.ListenAddress, conf.ListenPort)
-	listener, err := net.Listen("tcp", listenAddr)
+	listener, err := net.Listen("tcp", conf.BindAddress)
 	if err != nil {
 		return err
 	}
-	log.Debugf("Listening for %s", listenAddr)
+	log.Printf("Listen: %s", conf.BindAddress)
 
 	for {
 		nConn, err := listener.Accept()
@@ -36,14 +42,103 @@ func StartServer(conf Config) error {
 			return err
 		}
 
-		log.Debugf("Accept a client from %s", nConn.RemoteAddr())
+		log.Printf("Accepted client from %s", nConn.RemoteAddr())
 		go func() {
 			err := handleClient(conf, sConf, swift, nConn)
-			if err != nil {
+			if err == nil {
+				return
+			}
+
+			serr, ok := err.(*ssh.ServerAuthError)
+			if !ok {
 				log.Warnf("Client error: %v", err)
+				return
+			}
+
+			for _, err = range serr.Errors {
+				log.Warnf("Client Error: %v", err)
 			}
 		}()
 	}
+}
+
+// Generate ECDSA private key
+func generatePrivateKey(path string) error {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return err
+	}
+	encoded, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		return err
+	}
+
+	data := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: encoded})
+
+	return ioutil.WriteFile(path, data, 0600)
+}
+
+func initServer(conf Config) (sConf *ssh.ServerConfig, err error) {
+	// generate host key if it not exists.
+	if _, err = os.Stat(conf.HostPrivateKeyPath); err != nil {
+		if err = generatePrivateKey(conf.HostPrivateKeyPath); err != nil {
+			return nil, err
+		}
+	}
+
+	authPkey := func(c ssh.ConnMetadata, pkey ssh.PublicKey) (*ssh.Permissions, error) {
+		authorizedKeysBytes, err := ioutil.ReadFile(conf.AuthorizedKeysPath)
+		if err != nil {
+			return nil, err
+		}
+
+		authorizedKeysMap := map[string]bool{}
+		for len(authorizedKeysBytes) > 0 {
+			pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+			if err != nil {
+				return nil, err
+			}
+
+			authorizedKeysMap[string(pubKey.Marshal())] = true
+			authorizedKeysBytes = rest
+		}
+
+		if authorizedKeysMap[string(pkey.Marshal())] {
+			return &ssh.Permissions{
+				// Record the public key used for authentication.
+				Extensions: map[string]string{
+					"pubkey-fp": ssh.FingerprintSHA256(pkey),
+				},
+			}, nil
+		}
+		return nil, fmt.Errorf("unknown public key for %q", c.User())
+	}
+
+	authPassword := func(c ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+		if c.User() == "hiro" && string(password) == "test123" {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("password rejected for %q", c.User())
+	}
+
+	sConf = &ssh.ServerConfig{
+		PasswordCallback:  authPassword,
+		PublicKeyCallback: authPkey,
+	}
+
+	// host private key
+	pkeyBytes, err := ioutil.ReadFile(conf.HostPrivateKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	pkey, err := ssh.ParsePrivateKey(pkeyBytes)
+	if err != nil {
+		return nil, err
+	}
+	sConf.AddHostKey(pkey)
+
+	return sConf, nil
 }
 
 func handleClient(conf Config, sConf *ssh.ServerConfig, swift *Swift, nConn net.Conn) error {
@@ -87,54 +182,4 @@ func handleClient(conf Config, sConf *ssh.ServerConfig, swift *Swift, nConn net.
 		}
 	}
 	return nil
-}
-
-func serverConfig(conf Config) (sConf *ssh.ServerConfig, err error) {
-	authorizedKeysBytes, err := ioutil.ReadFile(conf.AuthorizedKeysPath)
-	if err != nil {
-		return nil, err
-	}
-
-	authorizedKeysMap := map[string]bool{}
-	for len(authorizedKeysBytes) > 0 {
-		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
-		if err != nil {
-			return nil, err
-		}
-
-		authorizedKeysMap[string(pubKey.Marshal())] = true
-		authorizedKeysBytes = rest
-	}
-
-	sConf = &ssh.ServerConfig{
-		PasswordCallback: func(c ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
-
-		PublicKeyCallback: func(c ssh.ConnMetadata, pkey ssh.PublicKey) (*ssh.Permissions, error) {
-			if authorizedKeysMap[string(pkey.Marshal())] {
-				return &ssh.Permissions{
-					// Record the public key used for authentication.
-					Extensions: map[string]string{
-						"pubkey-fp": ssh.FingerprintSHA256(pkey),
-					},
-				}, nil
-			}
-			return nil, fmt.Errorf("unknown public key for %q", c.User())
-		},
-	}
-
-	// private key of server
-	pkeyBytes, err := ioutil.ReadFile(conf.ServerPrivateKeyPath)
-	if err != nil {
-		return nil, err
-	}
-
-	pkey, err := ssh.ParsePrivateKey(pkeyBytes)
-	if err != nil {
-		return nil, err
-	}
-	sConf.AddHostKey(pkey)
-
-	return sConf, nil
 }
